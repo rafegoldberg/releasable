5f793e32c2619a205388251cb28af2f325263402
Rafe Goldberg <rafegoldberg@gmail.com>
2020-07-05 13:53:05 -0700

    chore: force workflow run

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,6 +3,7 @@ const [,,typeEnum] = require('@commitlint/config-angular').rules['type-enum'];
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
+    'subject-empty': [0, 'always'],
     'header-max-length': [0, 'always', 106],
     'type-enum': [2, 'always', [...typeEnum, 'chore', 'ref']],
   },
