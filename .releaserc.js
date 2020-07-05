784e1eb6186dcb64fc3788097a2c8d051ef14f11
Rafe Goldberg <rafegoldberg@gmail.com>
2020-07-05 14:58:58 -0700

    chore: clean up release config

--- .releaserc.js
+++ .releaserc.js
@@ -12,22 +12,10 @@ module.exports = {
       {
         preset: "angular",
         releaseRules: [
-          {
-            type: "feat",
-            release: "minor",
-          },
-          {
-            type: "style",
-            release: "patch",
-          },
-          {
-            type: "refactor",
-            release: "patch",
-          },
-          {
-            type: "ref",
-            release: "patch",
-          },
+          { type: "feat", release: "minor" },
+          { type: "style", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { scope: "skip", release: false },
         ],
         parserOpts: {
           noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
@@ -44,54 +32,20 @@ module.exports = {
         presetConfig: {
           header: "Testing",
           types: [
-            {
-              type: "feat",
-              section: "✨ New in this Release",
-              hidden: false,
-            },
-            {
-              type: "refactor",
-              section: "✨ New in this Release",
-              hidden: false,
-            },
-            {
-              type: "style",
-              section: "💄 Style & UI Improvements",
-              hidden: false,
-            },
-            {
-              type: "chore",
-              section: "🛠 Assorted Updates",
-              hidden: false,
-            },
-            {
-              type: "fix",
-              section: "🛠 Assorted Updates",
-              hidden: false,
-            },
-            {
-              type: "perf",
-              section: "🛠 Assorted Updates",
-              hidden: true,
-            },
-            {
-              type: "test",
-              section: "🛠 Assorted Updates",
-              hidden: true,
-            },
-            {
-              type: "docs",
-              section: "📘 Docs",
-              hidden: false,
-            },
-            {
-              type: "build",
-              hidden: true,
-            },
-            {
-              type: "ci",
-              hidden: true,
-            },
+            { type: "feat",     section: "✨ New & Improved", hidden: false },
+            { type: "refactor", section: "✨ New & Improved", hidden: true },
+
+            { type: "style",    section: "💄 Style Refinements", hidden: false },
+
+            { type: "fix",      section: "🛠 Fixes & Updates", hidden: false },
+            { type: "perf",     section: "🛠 Fixes & Updates", hidden: false },
+            { type: "chore",    section: "🛠 Fixes & Updates", hidden: true },
+            { type: "test",     section: "🛠 Fixes & Updates", hidden: true },
+
+            { type: "docs",     section: "📘 Docs", hidden: false },
+            
+            { type: "build", hidden: true, },
+            { type: "ci",    hidden: true, },
           ],
         },
       },
@@ -122,7 +76,8 @@ module.exports = {
       "@semantic-release/git",
       {
         assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
-        message: "Release v${nextRelease.version}\n\n${nextRelease.notes}\n\n[SKIP CI]",
+        message:
+          "Release v${nextRelease.version}\n\n${nextRelease.notes}\n\n[SKIP CI]",
       },
     ],
   ],
